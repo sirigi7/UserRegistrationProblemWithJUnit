2091e4c1da59105516ea9183d8459aeeaed2db84
sandeep <sirigi.reddy7@gmail.com>
2023-04-01 13:41:45 +0530

    Added: UC10_TestValidFirstname_LastName_Email

--- src/com/userregistrationproblemwithjunit/UserRegistrationProblemTest.java
+++ src/com/userregistrationproblemwithjunit/UserRegistrationProblemTest.java
@@ -1,7 +1,7 @@
 package com.userregistrationproblemwithjunit;
 
 import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 public class UserRegistrationProblemTest {
     UserRegistrationProblem userRegistrationProblem = new UserRegistrationProblem();
 
